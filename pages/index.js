c9ba81e258bed2281f194e7e710db303314ebf39
Felipe Rosas <contato@eufelipe.com>
2022-05-22 10:37:29 -0300

    chore: add login page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
+import { Flex, Button, Text, Image } from "@chakra-ui/react";
+import { FaGoogle } from "react-icons/fa";
+
 export default function Home() {
-  return <h2>Home</h2>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex width="100%" maxWidth={360} p="8" borderRadius={8} flexDir="column">
+        <Image src="/logo.png" alt="Novel" />
+
+        <Button width="100%" type="submit" mt="6" size="lg" onClick={() => {}}>
+          <FaGoogle color="#eba417" />
+          <Text ml="4" color="gray.650">
+            Sign in with Google
+          </Text>
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,13 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const styles = {
-  colors: {
-    brand: {
-      900: "#1a365d",
-      800: "#153e75",
-      700: "#2a69ac",
+export const theme = extendTheme({
+  styles: {
+    colors: {},
+    global: {
+      body: {
+        bg: "cyan.900",
+      },
     },
   },
-};
-
-export const theme = extendTheme(styles);
+});
