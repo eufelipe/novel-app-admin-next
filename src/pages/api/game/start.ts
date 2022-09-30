d6eef20325a33bee5bdba0d53ccdaeb5e8c39ca4
Felipe Rosas <felipe.rosas@g.globo>
2022-09-30 12:11:10 -0300

    chore: add random novels on start endpoint if empty date

--- src/pages/api/game/start.ts
+++ src/pages/api/game/start.ts
@@ -1,16 +1,30 @@
-import { methodNotAllowed, ok, badRequest } from "@helpers/http-helpers";
+import { badRequest, methodNotAllowed, ok } from "@helpers/http-helpers";
 import { loadNovelTodayService } from "@services/load-novels-today.service";
+import { loadNovelsService } from "@services/load-novels.service";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const ENABLE_RANDOM_IF_EMPTY_DATE =
+  process.env.ENABLE_RANDOM_IF_EMPTY_DATE === "true" || false;
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   if (request.method !== "GET") {
     return methodNotAllowed(response);
   }
   try {
-    const novel = await loadNovelTodayService();
-    return ok(response, novel);
+    const todayNovel = await loadNovelTodayService();
+
+    return ok(response, todayNovel);
   } catch (error) {
-    console.log(error);
+    if (ENABLE_RANDOM_IF_EMPTY_DATE) {
+      const data = await loadNovelsService();
+      const random = data.results.filter((item) => item.photos?.length >= 5);
+      const novel = random[Math.floor(Math.random() * random.length)];
+
+      if (novel) {
+        return ok(response, novel);
+      }
+    }
+
     return badRequest(response, { message: "no results" });
   }
 };
